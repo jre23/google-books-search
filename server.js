b31ed77f4ffba79932375dbb6c4ebe26e011e2e0
jre23 <jre.estrada@gmail.com>
2021-02-08 20:35:01 -0800

    set up initial file structure for the back-end, installed dependencies for full-stack

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+// define middleware
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+// serve static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// API routes
+// app.use();
+// send every other request to the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// set up connection to mongodb using mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }
+);
+// start the server
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
