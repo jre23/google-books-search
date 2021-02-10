e5c791e26a4e4b911997c9fc47a9291f636cc9b4
jre23 <jre.estrada@gmail.com>
2021-02-09 23:39:36 -0800

    added view and save buttons to results list with styling, view links to info on book

--- client/src/components/ResultsList.jsx
+++ client/src/components/ResultsList.jsx
@@ -7,14 +7,34 @@ const ResultsList = (props) => {
         <ul className="list-group" style={{ opacity: "0.88" }}>
           {props.results.map((res) => (
             <li className="list-group-item" key={res.id}>
-              <h5>{res.volumeInfo.title}</h5>
-              <img
-                alt={res.volumeInfo.title}
-                className="img-fluid pt-4 pr-4"
-                style={{ float: "left" }}
-                src={res.volumeInfo.imageLinks.smallThumbnail}
-              />
-              <h6 className="pt-4">{res.volumeInfo.description}</h6>
+              <div style={{ float: "left" }}>
+                <h5>{res.volumeInfo.title}</h5>
+                <h6>Written by {res.volumeInfo.authors}</h6>
+              </div>
+              <div>
+                <a className="btn btn-info" style={{ float: "right" }}>
+                  Save
+                </a>
+                <a
+                  href={res.volumeInfo.infoLink}
+                  rel="noreferrer"
+                  target="_blank"
+                  role="button"
+                  className="btn btn-success mr-1"
+                  style={{ float: "right", color: "white" }}
+                >
+                  View
+                </a>
+              </div>
+              <div className="pt-3" style={{ clear: "both" }}>
+                <img
+                  alt={res.volumeInfo.title}
+                  className="img-fluid pr-4"
+                  style={{ float: "left" }}
+                  src={res.volumeInfo.imageLinks.thumbnail}
+                />
+                <h6>{res.volumeInfo.description}</h6>
+              </div>
             </li>
           ))}
         </ul>
